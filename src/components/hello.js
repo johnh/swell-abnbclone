03fbb8e1b16a6750695f1eafe833d9ca8d4dba61
John Haro <john.haro@gmail.com>
2018-01-31 22:52:15 -0600

    first commit

---
+++ src/components/hello.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Hello extends React.Component {
+  render() {
+    // TODO: stuff
+    return <div>HELLLOOO from {this.props.lastName}, {this.props.firstName}</div>;
+  }
+}
+
+export default Hello;
